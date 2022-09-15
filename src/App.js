2eeee92c786b05e408b80e9117cc723435d38b3e
pabpabpab <urallove@yandex.ru>
2022-09-16 02:07:13 +0500

    first react app
    first react app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import './App.sass';
+import Message from './Message';
 
-function App() {
+function App(props) {
+  const myName = 'Alex';
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+          <header className="App-header">
+              <Message name={myName}/>
+              <h3 style={{color: '#2fc4ff', transform: 'rotate(-45deg)'}}>Привет, привет</h3>
+          </header>
+      </div>
   );
 }
 
